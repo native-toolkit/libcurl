7084474ab4eb76efa3ba1dc6eb4e407ae07a72ba
Joshua Granick <jgranick@users.noreply.github.com>
2015-07-10 11:14:41 -0700

    Disable axTLS non-blocking only if NATIVE_TOOLKIT_CURL_DISABLE_AXTLS_NONBLOCKING

--- src/vtls/axtls.h
+++ src/vtls/axtls.h
@@ -51,7 +51,9 @@ int Curl_axtls_check_cxn(struct connectdata *conn);
 #define curlssl_init Curl_axtls_init
 #define curlssl_cleanup Curl_axtls_cleanup
 #define curlssl_connect Curl_axtls_connect
-//#define curlssl_connect_nonblocking Curl_axtls_connect_nonblocking
+#ifndef NATIVE_TOOLKIT_CURL_DISABLE_AXTLS_NONBLOCKING
+#define curlssl_connect_nonblocking Curl_axtls_connect_nonblocking
+#endif
 #define curlssl_session_free(x)  Curl_axtls_session_free(x)
 #define curlssl_close_all Curl_axtls_close_all
 #define curlssl_close Curl_axtls_close
